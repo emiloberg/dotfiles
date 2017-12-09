f4536a76fa49ac78c39d2e97875d2208480d379a
Emil Oberg <emil@bitupnorth.com>
2017-12-09 17:16:10 +0100

    Add new way of saving Brew installed apps

---
+++ install-brew.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Installing apps, this is going to take some time..."
+
+brew bundle
+
+echo "All done!"
--- save-brew.sh
+++ save-brew.sh
@@ -1,6 +1,4 @@
 cd ~/code/github/dotfiles/
 echo "Saving Brew"
-brew leaves > brew.txt
-echo "Saving Brew Cask"
-brew cask list > brew-cask.txt
+brew bundle dump
 echo "Done"
