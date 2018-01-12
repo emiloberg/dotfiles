96beb5b25ca180dd32233008503d7644f82beb35
Emil Oberg <emil@bitupnorth.com>
2018-01-12 14:16:40 +0100

    Add vimrc

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -9,6 +9,7 @@ ln -sfv "$CURRENT_DIR/.gitconfig" ~
 ln -sfv "$CURRENT_DIR/.gitignore_global" ~
 ln -sfv "$CURRENT_DIR/.phoenix.js" ~
 ln -sfv "$CURRENT_DIR/.zshrc" ~
+ln -sfv "$CURRENT_DIR/.vimrc" ~
 mkdir -p ~/.config/karabiner && ln -sfv "$CURRENT_DIR/karabiner.json" ~/.config/karabiner
 
 echo "I am done!"
